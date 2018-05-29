95f7a1fe310a41439a4b52f946f2a8250d2c662f
Eric <eric688@gmail.com>
2018-05-29 15:03:59 +1200

    This now correctly calculates max upstroke rate

--- initialise.m
+++ initialise.m
@@ -1,4 +1,4 @@
-function [V,b_V,p_V,APD,max_upstroke,upstroke_V,upstroke_i,V_90,V_90_t] = initialise(V,t)
+function [V,b_V,p_V,APD,max_upstroke,upstroke_V,upstroke_i,V90,V_90_t] = initialise(V,t)
 
 % This function will take in an array of voltages representing membrane
 % potential and then output some constants to set as parameter defaults in
@@ -20,43 +20,33 @@ function [V,b_V,p_V,APD,max_upstroke,upstroke_V,upstroke_i,V_90,V_90_t] = initia
 % max_upstroke  scalar             maximum rate of upstroke
 % upstroke_V    scalar             membrane voltage at maximum upstroke
 % upstroke_i    scalar             index when maximum upstroke occurs
-% V90_i         scalar             indices at which 10% of max voltage
-%                                  occurs
+% V90           scalar             10% of baseline voltage to maximum voltage 
+% V90_t         scalar array       times at which V90 occurs
 
 
 % Finding baseline voltage and peak voltage
 [b_V, ~] = min(V);
 [p_V, p_i] = max(V);
 
-% Determine APD
-
-% First determine the first derivative
+% Find maximum upstroke
 diff_1 = diff(V);
 [max_upstroke, upstroke_i] = max(diff_1);
 upstroke_V = V(upstroke_i);
 
-% % Find the next time point where membrane potential returns to upstroke_V
-% upstroke_count = 0;
-% i = upstroke_i;
-% 
-% while upstroke_count <1
-%     if i > p_i
-%         if (V(i) < (upstroke_V + 1)) && (V(i) > (upstroke_V - 1))
-%             upstroke_count = 1;
-%         end
-%     end
-%     i = i+1;
-% end
-% 
-% %
+max_upstroke = max_upstroke/(t(upstroke_i+1)-t(upstroke_i));
 
-V_90 = p_V-(p_V - b_V)*0.9;
+%% Determine action potential duration
+
+% Determine V90 
+V90 = p_V-(p_V - b_V)*0.9;
+
+% Determine indices at which V90 occur
 V_90_i = [0,0];
-[~,V_90_i(1)] = min(abs(V(1:p_i)-V_90));
-[~,V_90_i(2)] = min(abs(V(p_i:end)-V_90));
-V_90_i(2) = V_90_i(2) + p_i;
-V_90_t = t(V_90_i);
+[~,V_90_i(1)] = min(abs(V(1:p_i)-V90));     % Index prior to peak voltage
+[~,V_90_i(2)] = min(abs(V(p_i:end)-V90));   % Index after peak voltage
+V_90_i(2) = V_90_i(2) + p_i;            
+V_90_t = t(V_90_i);                         % Convert to time
 
-APD = V_90_t(2) - V_90_t(1);
+APD = V_90_t(2) - V_90_t(1);                % Calculate APD
 
 end
